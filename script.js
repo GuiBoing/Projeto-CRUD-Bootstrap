c7d8c23784fa3b1d65e6fe84357d7a96bf321fa6
guilhermeboing <guilhermeboing@pactosolucoes.com.br>
2023-11-14 15:01:06 -0300

    Revert "iniciado scripts para banco de dados"
    This reverts commit bd8473783d5f36b757bb7e2f1b3af85038a6d205.

--- script.js
+++ script.js
@@ -64,210 +64,194 @@ var contactList = [
     },
 ];
 
-let db;
-
-console.log(db);
-
 window.onload = function () {
     tableGenerator();
-    CreateDatabase();
+}
 
-    function CreateDatabase() {
-        const request = indexedDB.open("CRUD-DB");
-        request.onerror = (event) => {
-            console.error("Why didn't you allow my web app to use IndexedDB?!");
-        };
-        request.onsuccess = (event) => {
-            console.info("conected to CRUD-DB local database")
-            db = event.target.result;
-            const objectStore = db.createObjectStore("name", { keyPath: "myKey" });
-        };
-    }
+function GRUDContacts(type, manipulatedID) {
+    var manipulatedArray = ''
+    switch (type) {
+        case (type = "new"):
+            $("#nav-form-tab").click();
+            document.getElementById("NameInput").value = '';
+            document.getElementById("NameInput").removeAttribute("readonly");
+            document.getElementById("LastNameInput").value = '';
+            document.getElementById("LastNameInput").removeAttribute("readonly");
+            document.getElementById("EmailInput").value = '';
+            document.getElementById("EmailInput").removeAttribute("readonly");
+            document.getElementById("PhoneInput").value = '';
+            document.getElementById("PhoneInput").removeAttribute("readonly");
+            document.getElementById("AltPhoneInput").value = '';
+            document.getElementById("AltPhoneInput").removeAttribute("readonly");
+            document.getElementById("DescriptionTextArea").value = '';
+            document.getElementById("DescriptionTextArea").removeAttribute("readonly");
+            document.getElementById("submitButton").removeAttribute("disabled");
+            document.getElementById("submitButton").setAttribute("onclick", "GRUDContacts('confirmNew')");
+            break
+        case (type = "confirmNew"):
 
-    function GRUDContacts(type, manipulatedID) {
-        var manipulatedArray = ''
-        switch (type) {
-            case (type = "new"):
-                $("#nav-form-tab").click();
-                document.getElementById("NameInput").value = '';
-                document.getElementById("NameInput").removeAttribute("readonly");
-                document.getElementById("LastNameInput").value = '';
-                document.getElementById("LastNameInput").removeAttribute("readonly");
-                document.getElementById("EmailInput").value = '';
-                document.getElementById("EmailInput").removeAttribute("readonly");
-                document.getElementById("PhoneInput").value = '';
-                document.getElementById("PhoneInput").removeAttribute("readonly");
-                document.getElementById("AltPhoneInput").value = '';
-                document.getElementById("AltPhoneInput").removeAttribute("readonly");
-                document.getElementById("DescriptionTextArea").value = '';
-                document.getElementById("DescriptionTextArea").removeAttribute("readonly");
-                document.getElementById("submitButton").removeAttribute("disabled");
-                document.getElementById("submitButton").setAttribute("onclick", "GRUDContacts('confirmNew')");
-                break
-            case (type = "confirmNew"):
+            var someone = {
+                id: 1 + contactList.length,
+                Name: document.getElementById("NameInput").value,
+                LastName: document.getElementById("LastNameInput").value,
+                Email: document.getElementById("EmailInput").value,
+                Phone: document.getElementById("PhoneInput").value,
+                AltPhone: document.getElementById("AltPhoneInput").value,
+                Description: document.getElementById("DescriptionTextArea").value,
+            };
 
-                var someone = {
-                    id: 1 + contactList.length,
-                    Name: document.getElementById("NameInput").value,
-                    LastName: document.getElementById("LastNameInput").value,
-                    Email: document.getElementById("EmailInput").value,
-                    Phone: document.getElementById("PhoneInput").value,
-                    AltPhone: document.getElementById("AltPhoneInput").value,
-                    Description: document.getElementById("DescriptionTextArea").value,
-                };
-
-                if (contactList.length !== 0) {
-                    var lastPerson = contactList.length;
-                    if (contactList[lastPerson - 1].Name != someone.Name) {
-                        contactList.push(someone);
-                        tableGenerator();
-                        ShowModalId("CreatedModal");
-                    } else {
-                        ShowModalId("FailModal");
-                    }
-                } else {
+            if (contactList.length !== 0) {
+                var lastPerson = contactList.length;
+                if (contactList[lastPerson - 1].Name != someone.Name) {
                     contactList.push(someone);
                     tableGenerator();
                     ShowModalId("CreatedModal");
-                }
-
-                GRUDContacts('new',)
-
-                break;
-            case (type = "edit"):
-                document.getElementById("submitButton").setAttribute("onclick", "GRUDContacts('confirmEdit'," + manipulatedID + ")"
-                );
-
-                contactList.forEach((contact, index) => {
-                    if (contact.id == manipulatedID) {
-                        manipulatedArray = index
-                    }
-                })
-
-                $("#nav-form-tab").click();
-                document.getElementById("NameInput").value = contactList[manipulatedArray].Name;
-                document.getElementById("NameInput").removeAttribute("readonly");
-
-                document.getElementById("LastNameInput").value = contactList[manipulatedArray].LastName;
-                document.getElementById("LastNameInput").removeAttribute("readonly");
-
-                document.getElementById("EmailInput").value = contactList[manipulatedArray].Email;
-                document.getElementById("EmailInput").removeAttribute("readonly");
-
-                document.getElementById("PhoneInput").value = contactList[manipulatedArray].Phone;
-                document.getElementById("PhoneInput").removeAttribute("readonly");
-
-                document.getElementById("AltPhoneInput").value = contactList[manipulatedArray].AltPhone;
-                document.getElementById("AltPhoneInput").removeAttribute("readonly");
-
-                document.getElementById("DescriptionTextArea").value = contactList[manipulatedArray].Description;
-                document.getElementById("DescriptionTextArea").removeAttribute("readonly");
-
-                document.getElementById("submitButton").removeAttribute("disabled");
-
-
-                break;
-            case (type = "confirmEdit"):
-
-                contactList.forEach((contact, index) => {
-                    if (contact.id == manipulatedID) {
-                        manipulatedArray = index
-                    }
-                })
-
-
-
-                contactList[manipulatedArray] = {
-                    id: manipulatedID,
-                    Name: document.getElementById("NameInput").value,
-                    LastName: document.getElementById("LastNameInput").value,
-                    Email: document.getElementById("EmailInput").value,
-                    Phone: document.getElementById("PhoneInput").value,
-                    AltPhone: document.getElementById("AltPhoneInput").value,
-                    Description: document.getElementById("DescriptionTextArea").value,
-                };
-
-                tableGenerator();
-                ShowModalId("EditedModal");
-                GRUDContacts("new", "");
-
-                break;
-            case (type = "detail"):
-                contactList.forEach((contact, index) => {
-                    if (contact.id == manipulatedID) {
-                        manipulatedArray = index
-                    }
-                })
-                $("#nav-form-tab").click();
-                document.getElementById("NameInput").value = contactList[manipulatedArray].Name;
-                document.getElementById("NameInput").setAttribute("readonly", true);
-
-                document.getElementById("LastNameInput").value = contactList[manipulatedArray].LastName;
-                document.getElementById("LastNameInput").setAttribute("readonly", true);
-
-                document.getElementById("EmailInput").value = contactList[manipulatedArray].Email;
-                document.getElementById("EmailInput").setAttribute("readonly", true);
-
-                document.getElementById("PhoneInput").value = contactList[manipulatedArray].Phone;
-                document.getElementById("PhoneInput").setAttribute("readonly", true);
-
-                document.getElementById("AltPhoneInput").value = contactList[manipulatedArray].AltPhone;
-                document.getElementById("AltPhoneInput").setAttribute("readonly", true);
-
-                document.getElementById("DescriptionTextArea").value = contactList[manipulatedArray].Description;
-                document.getElementById("DescriptionTextArea").setAttribute("readonly", true);
-
-                document.getElementById("submitButton").setAttribute("disabled", true);
-
-
-                break;
-            case (type = "remove"):
-                let indexRemove = 0
-                var toremove = []
-                var checkboxes = document.getElementsByClassName('form-check-input')
-                for (var i = 0; i < checkboxes.length; i++) {
-                    if (checkboxes[i].checked == true && i != 0 && i != null) {
-                        toremove[indexRemove] = checkboxes[i].value
-                        indexRemove = indexRemove + 1
-                    }
-                }
-                GRUDContacts('confirmRemove', toremove);
-
-
-                break;
-            case (type = "confirmRemove"):
-                console.log(manipulatedID.constructor);
-
-                if (manipulatedID.constructor === Array) {
-                    manipulatedID.forEach((idToRemove) => {
-                        contactList.forEach((contact, index) => {
-                            if (contact.id == idToRemove) {
-                                contactList.splice(index, 1);
-                            }
-                        })
-                    })
                 } else {
+                    ShowModalId("FailModal");
+                }
+            } else {
+                contactList.push(someone);
+                tableGenerator();
+                ShowModalId("CreatedModal");
+            }
+
+            GRUDContacts('new',)
+
+            break;
+        case (type = "edit"):
+            document.getElementById("submitButton").setAttribute("onclick", "GRUDContacts('confirmEdit'," + manipulatedID + ")"
+            );
+
+            contactList.forEach((contact, index) => {
+                if (contact.id == manipulatedID) {
+                    manipulatedArray = index
+                }
+            })
+
+            $("#nav-form-tab").click();
+            document.getElementById("NameInput").value = contactList[manipulatedArray].Name;
+            document.getElementById("NameInput").removeAttribute("readonly");
+
+            document.getElementById("LastNameInput").value = contactList[manipulatedArray].LastName;
+            document.getElementById("LastNameInput").removeAttribute("readonly");
+
+            document.getElementById("EmailInput").value = contactList[manipulatedArray].Email;
+            document.getElementById("EmailInput").removeAttribute("readonly");
+
+            document.getElementById("PhoneInput").value = contactList[manipulatedArray].Phone;
+            document.getElementById("PhoneInput").removeAttribute("readonly");
+
+            document.getElementById("AltPhoneInput").value = contactList[manipulatedArray].AltPhone;
+            document.getElementById("AltPhoneInput").removeAttribute("readonly");
+
+            document.getElementById("DescriptionTextArea").value = contactList[manipulatedArray].Description;
+            document.getElementById("DescriptionTextArea").removeAttribute("readonly");
+
+            document.getElementById("submitButton").removeAttribute("disabled");
+
+
+            break;
+        case (type = "confirmEdit"):
+
+            contactList.forEach((contact, index) => {
+                if (contact.id == manipulatedID) {
+                    manipulatedArray = index
+                }
+            })
+
+
+
+            contactList[manipulatedArray] = {
+                id: manipulatedID,
+                Name: document.getElementById("NameInput").value,
+                LastName: document.getElementById("LastNameInput").value,
+                Email: document.getElementById("EmailInput").value,
+                Phone: document.getElementById("PhoneInput").value,
+                AltPhone: document.getElementById("AltPhoneInput").value,
+                Description: document.getElementById("DescriptionTextArea").value,
+            };
+
+            tableGenerator();
+            ShowModalId("EditedModal");
+            GRUDContacts("new", "");
+
+            break;
+        case (type = "detail"):
+            contactList.forEach((contact, index) => {
+                if (contact.id == manipulatedID) {
+                     manipulatedArray = index
+                }
+                })
+            $("#nav-form-tab").click();
+            document.getElementById("NameInput").value = contactList[manipulatedArray].Name;
+            document.getElementById("NameInput").setAttribute("readonly", true);
+
+            document.getElementById("LastNameInput").value = contactList[manipulatedArray].LastName;
+            document.getElementById("LastNameInput").setAttribute("readonly", true);
+
+            document.getElementById("EmailInput").value = contactList[manipulatedArray].Email;
+            document.getElementById("EmailInput").setAttribute("readonly", true);
+
+            document.getElementById("PhoneInput").value = contactList[manipulatedArray].Phone;
+            document.getElementById("PhoneInput").setAttribute("readonly", true);
+
+            document.getElementById("AltPhoneInput").value = contactList[manipulatedArray].AltPhone;
+            document.getElementById("AltPhoneInput").setAttribute("readonly", true);
+
+            document.getElementById("DescriptionTextArea").value = contactList[manipulatedArray].Description;
+            document.getElementById("DescriptionTextArea").setAttribute("readonly", true);
+
+            document.getElementById("submitButton").setAttribute("disabled", true);
+
+
+            break;
+        case (type = "remove"):
+            let indexRemove = 0
+            var toremove = []
+            var checkboxes = document.getElementsByClassName('form-check-input')
+            for (var i = 0; i < checkboxes.length; i++) {
+                if (checkboxes[i].checked == true && i != 0 && i != null) {
+                    toremove[indexRemove] = checkboxes[i].value
+                    indexRemove = indexRemove + 1
+                }
+            }
+            GRUDContacts('confirmRemove', toremove);
+
+
+            break;
+        case (type = "confirmRemove"):
+            console.log(manipulatedID.constructor);
+
+            if (manipulatedID.constructor === Array) {
+                manipulatedID.forEach((idToRemove) => {
                     contactList.forEach((contact, index) => {
-                        if (contact.id == manipulatedID) {
+                        if (contact.id == idToRemove) {
                             contactList.splice(index, 1);
                         }
+                    })
+                })
+            } else {
+                contactList.forEach((contact, index) => {
+                    if (contact.id == manipulatedID) {
+                        contactList.splice(index, 1);
                     }
-                    )
                 }
+                )
+            }
 
-                tableGenerator()
-                break;
-        }
+            tableGenerator()
+            break;
     }
+}
 
-    function ShowModalId(ModalID) {
-        $("#" + ModalID + "").modal("show");
-    }
+function ShowModalId(ModalID) {
+    $("#" + ModalID + "").modal("show");
+}
 
-    function tableGenerator() {
-        var tablerow = ``;
-        contactList.forEach((value) => {
-            tablerow += `<tr>
+function tableGenerator() {
+    var tablerow = ``;
+    contactList.forEach((value) => {
+        tablerow += `<tr>
         <td>
         <input class="form-check-input" type="checkbox" value="${value.id}">
         </td>
@@ -287,14 +271,13 @@ window.onload = function () {
         <td class="d-md-table-cell d-none">${value.AltPhone}</td>
         <td class="d-md-table-cell d-none">${value.Description}</td>
         </tr>`
-        });
-        document.getElementById("answer").innerHTML = tablerow;
-    }
+    });
+    document.getElementById("answer").innerHTML = tablerow;
+}
 
-    function getMask(id) {
-        $("#" + id).mask("(00) 0 0000-0000")
-    }
-    function none() {
-        console.log('.')
-    }
+function getMask(id) {
+    $("#" + id).mask("(00) 0 0000-0000")
+}
+function none(){
+    console.log('.')
 }
